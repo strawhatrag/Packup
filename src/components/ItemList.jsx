5375e5e9f3d2efaa2c47ec242137c85e058362b8
strawhatrag <sreeragbharathan791@gmail.com>
2024-05-18 22:37:02 +0530

    sortby working fine

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,23 +1,61 @@
 import EmptyView from "./EmptyView";
+import Select from "react-select";
+import { useState } from "react";
+
+const sortingOptions = [
+  {
+    label: "Sort by Default",
+    value: "default",
+  },
+  {
+    label: "Sort by Packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by Unpacked",
+    value: "unpacked",
+  },
+];
 
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    } else if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    } else {
+      return 0; // Explicitly return 0 to keep the original order if sortBy is "default"
+    }
+  });
+
   return (
     <ul className="item-list">
       {items.length === 0 && <EmptyView />}
-      {items.map((item) => {
-        return (
-          <Item
-            key={item.id}
-            item={item}
-            onDeleteItem={handleDeleteItem}
-            onToggleItem={handleToggleItem}
+
+      {items.length > 0 && (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
           />
-        );
-      })}
+        </section>
+      )}
+
+      {sortedItems.map((item) => (
+        <Item
+          key={item.id}
+          item={item}
+          onDeleteItem={handleDeleteItem}
+          onToggleItem={handleToggleItem}
+        />
+      ))}
     </ul>
   );
 }
